d95f490f21d67660fe992d16c018d685edf6aac4
chocolateimage <chocolateimage@protonmail.com>
2025-07-25 17:51:28 +0200

    Improve colors on windows

--- qrhythmcafe.py
+++ qrhythmcafe.py
@@ -260,6 +260,7 @@ if __name__ == "__main__":
         palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
         palette.setColor(QPalette.ColorRole.Base, QColor(25, 25, 25))
         palette.setColor(QPalette.ColorRole.AlternateBase, QColor(53, 53, 53))
+        palette.setColor(QPalette.ColorRole.Light, QColor(80, 80, 80))
         palette.setColor(QPalette.ColorRole.ToolTipBase, Qt.GlobalColor.black)
         palette.setColor(QPalette.ColorRole.ToolTipText, Qt.GlobalColor.white)
         palette.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.white)
@@ -269,6 +270,7 @@ if __name__ == "__main__":
         palette.setColor(QPalette.ColorRole.Link, QColor(42, 130, 218))
         palette.setColor(QPalette.ColorRole.Highlight, QColor(42, 130, 218))
         palette.setColor(QPalette.ColorRole.HighlightedText, Qt.GlobalColor.black)
+        palette.setColor(QPalette.ColorRole.Accent, QColor(42, 130, 218))
         app.setPalette(palette)
 
     mw = MainWindow()
