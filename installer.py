048f8d986ce727b6585fae9cf42f3e5d206e81ee
chocolateimage <chocolateimage@protonmail.com>
2025-08-01 21:12:37 +0200

    Add image to installer window

--- installer.py
+++ installer.py
@@ -6,6 +6,29 @@ import utils
 import traceback
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QMessageBox
+from PyQt6.QtGui import QPixmap
+
+
+class ImageWidget(QLabel):
+    def __init__(self, parent, pixmap: QPixmap):
+        super().__init__(parent)
+
+        self.originalPixmap = pixmap
+        self.setMinimumSize(1, 1)
+        self.generatePixmap()
+
+    def resizeEvent(self, a0):
+        self.generatePixmap()
+        return super().resizeEvent(a0)
+
+    def generatePixmap(self):
+        self.setPixmap(
+            self.originalPixmap.scaled(
+                self.size(),
+                Qt.AspectRatioMode.KeepAspectRatioByExpanding,
+                Qt.TransformationMode.SmoothTransformation,
+            )
+        )
 
 
 class InstallWindow(QWidget):
@@ -13,7 +36,7 @@ class InstallWindow(QWidget):
         super().__init__()
 
         self.setWindowTitle("Level Installer")
-        self.resize(500, 150)
+        self.resize(500, 200)
 
         if not os.path.exists(file):
             QMessageBox.critical(None, "File not found", f"File {file} was not found")
@@ -37,17 +60,38 @@ class InstallWindow(QWidget):
         )
 
         self.vlay = QVBoxLayout(self)
+        self.vlay.setContentsMargins(0, 0, 0, 0)
+        self.vlay.setSpacing(0)
+
+        try:
+            previewImage = self.zipfile.read(levelContents["settings"]["previewImage"])
+            previewPixmap = QPixmap()
+            previewPixmap.loadFromData(previewImage)
+        except Exception:
+            previewPixmap = None
+
+        if previewPixmap is not None and not previewPixmap.isNull():
+            self.imageWidget = ImageWidget(self, previewPixmap)
+            self.imageWidget.setFixedHeight(80)
+            self.vlay.addWidget(self.imageWidget)
+
+        self.innerLay = QVBoxLayout()
+        self.innerLay.setContentsMargins(9, 9, 9, 9)
+        self.innerLay.setSpacing(9)
+        self.vlay.addLayout(self.innerLay, 1)
 
         self.messageLabel = QLabel(
-            f"Do you want to install {levelContents['settings']['song']} created by {levelContents['settings']['author']}?",
+            f"Do you want to install <b>{levelContents['settings']['song']}</b> created by <b>{levelContents['settings']['author']}</b>?",
             self,
         )
         self.messageLabel.setAlignment(Qt.AlignmentFlag.AlignTop)
-        self.vlay.addWidget(self.messageLabel)
+        self.innerLay.addWidget(self.messageLabel)
 
         self.installButton = QPushButton("Install", self)
         self.installButton.clicked.connect(self.install)
-        self.vlay.addWidget(self.installButton, alignment=Qt.AlignmentFlag.AlignRight)
+        self.innerLay.addWidget(
+            self.installButton, alignment=Qt.AlignmentFlag.AlignRight
+        )
 
         if os.path.exists(self.levelpath):
             self.markInstalled()
